a7cb46bc977fc8e295fd6f4e2a0d35eeacc25732
Ashraful Islam <ashrafulislamdarkeye@gmail.com>
2026-02-27 20:46:09 +0600

    Email Ptotocall changed from smtp to resend api

--- util/otp.go
+++ util/otp.go
@@ -4,7 +4,9 @@ import (
 	"crypto/rand"
 	"fmt"
 	"log"
-	"net/smtp"
+	"os"
+
+	"github.com/resend/resend-go/v2"
 )
 
 // GenerateOTP returns a 6-digit numeric OTP
@@ -23,23 +25,36 @@ func GenerateOTP() string {
 }
 
 // SendOTPEmail sends an email with the OTP
+// func SendOTPEmail(toEmail, otp string) error {
+// 	auth := smtp.PlainAuth(
+// 		"",
+// 		"ashrafulialamraju@gmail.com",
+// 		"uvanmhqruwesrnhc",
+// 		"smtp.gmail.com",
+// 	)
+
+// 	msg := otp
+
+// 	err := smtp.SendMail(
+// 		"smtp.gmail.com:587",
+// 		auth,
+// 		"ashrafulialamraju@gmail.com",
+// 		[]string{"ashrafulialamraju@gmail.com"},
+// 		[]byte(msg),
+// 	)
+
+//		return err
+//	}
 func SendOTPEmail(toEmail, otp string) error {
-	auth := smtp.PlainAuth(
-		"",
-		"ashrafulialamraju@gmail.com",
-		"uvanmhqruwesrnhc",
-		"smtp.gmail.com",
-	)
+	client := resend.NewClient(os.Getenv("RESEND_API_KEY"))
 
-	msg := otp
+	params := &resend.SendEmailRequest{
+		From:    "onboarding@resend.dev",
+		To:      []string{toEmail},
+		Subject: "Your OTP Code",
+		Text:    "Your OTP is: " + otp,
+	}
 
-	err := smtp.SendMail(
-		"smtp.gmail.com:587",
-		auth,
-		"ashrafulialamraju@gmail.com",
-		[]string{"ashrafulialamraju@gmail.com"},
-		[]byte(msg),
-	)
-
+	_, err := client.Emails.Send(params)
 	return err
 }
